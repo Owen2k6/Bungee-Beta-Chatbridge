b0e65896f4162944ecd508a26283ec05fb59ae8a
Owen2k6 <fulptdm@gmail.com>
2022-12-21 22:25:50 +0000

    Update BungeePlugin.java

--- src/main/java/com/oldschoolminecraft/cb/BungeePlugin.java
+++ src/main/java/com/oldschoolminecraft/cb/BungeePlugin.java
@@ -3,6 +3,8 @@ package com.oldschoolminecraft.cb;
 import net.md_5.bungee.api.event.ChatEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerChatEvent;
 
 public class BungeePlugin extends Plugin implements Listener
 {
@@ -12,11 +14,17 @@ public class BungeePlugin extends Plugin implements Listener
         getProxy().getPluginManager().registerListener(this, this);
     }
 
-    public void onChat(ChatEvent event)
-    {
+//    public void onChat(ChatEvent event)
+//    {
+//        getProxy().getServers().values().forEach(server ->
+//                server.getPlayers().forEach(player ->
+//                        player.sendMessage(event.getMessage())));
+//    }
+    @EventHandler
+    public void onMessageSend(PlayerChatEvent event){
         getProxy().getServers().values().forEach(server ->
                 server.getPlayers().forEach(player ->
-                        player.sendMessage(event.getMessage())));
+                        player.sendMessage(event.getPlayer().getName()+event.getMessage())));
     }
 
     @Override
