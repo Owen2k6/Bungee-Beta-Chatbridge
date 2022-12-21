15b85f5f59e61f0b927ab416facad6638ca003ea
moderatorman <12193288+moderatorman@users.noreply.github.com>
2022-12-21 14:15:24 -0800

    Plugin code

---
+++ src/main/java/com/oldschoolminecraft/cb/BukkitPlugin.java
@@ -0,0 +1,18 @@
+package com.oldschoolminecraft.cb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitPlugin extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
---
+++ src/main/java/com/oldschoolminecraft/cb/BungeePlugin.java
@@ -0,0 +1,27 @@
+package com.oldschoolminecraft.cb;
+
+import net.md_5.bungee.api.event.ChatEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeePlugin extends Plugin implements Listener
+{
+    @Override
+    public void onEnable()
+    {
+        getProxy().getPluginManager().registerListener(this, this);
+    }
+
+    public void onChat(ChatEvent event)
+    {
+        getProxy().getServers().values().forEach(server ->
+                server.getPlayers().forEach(player ->
+                        player.sendMessage(event.getMessage())));
+    }
+
+    @Override
+    public void onDisable()
+    {
+        //
+    }
+}
